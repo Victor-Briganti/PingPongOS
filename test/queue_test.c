52ad1b534d5995aa2b3623c17f08214246a52a42
Victor Briganti <jvbriganti@gmail.com>
2024-09-09 16:02:31 -0300

    [Queue][Test] Refactor tests of the queue
    Separate the test into smaller pieces, added various insert and remove
    tests.

--- test/queue_test.c
+++ test/queue_test.c
@@ -1,14 +1,12 @@
 // PingPongOS - PingPong Operating System
-// Prof. Carlos A. Maziero, DINF UFPR
-// Versão 1.4 -- Janeiro de 2022
-// Teste da implementação de fila genérica queue.c/queue.h.
-
-// ESTE ARQUIVO NÃO DEVE SER MODIFICADO - ELE SERÁ SOBRESCRITO NOS TESTES
+// Victor Briganti
+// Versão 0.1 -- September 2024
+// Test the implementation of the generic queue queue.c/queue.h.
 
 // operating system check
 #if defined(_WIN32) || (!defined(__unix__) && !defined(__unix) &&              \
                         (!defined(__APPLE__) || !defined(__MACH__)))
-#warning Este código foi planejado para ambientes UNIX (LInux, *BSD, MacOS). A compilação e execução em outros ambientes é responsabilidade do usuário.
+#warning This code is developed to work in UNIX systems. It may not work as intended on other systems.
 #endif
 
 #include "queue.h"
@@ -18,308 +16,356 @@
 
 #define N 100
 
-// A estrutura "filaint_t" será usada com as funções de queue.c usando um
-// casting para o tipo "queue_t". Isso funciona bem, se os campos iniciais
-// de ambas as estruturas forem os mesmos. De acordo com a seção 6.7.2.1 do
-// padrão C99: "Within a structure object, the non-bit-ﬁeld members and the
-// units in which bit-ﬁelds reside have addresses that increase in the order
-// in which they are declared.".
+// The structure "queueint_t" is going to be used with the functions in queue.c,
+// it should be used by a casting with the "queue_t". This only works if the
+// first fields are the same between the structures. This is true if we follow
+// the 6.7.2.1 of the C99 standard:"Within a structure object, the non-bit-ﬁeld
+// members and the units in which bit-ﬁelds reside have addresses that increase
+// in the order in which they are declared.".
 
-typedef struct filaint_t {
-  struct filaint_t *prev; // ptr para usar cast com queue_t
-  struct filaint_t *next; // ptr para usar cast com queue_t
-  int id;
-  // outros campos podem ser acrescidos aqui
-} filaint_t;
-
-filaint_t item[N];
-filaint_t *fila0, *fila1, *aux, *final;
-int ret;
+typedef struct queueint_t {
+  struct queueint_t *prev; // ptr that is going to be casted to queue_t
+  struct queueint_t *next; // ptr that is going to be casted to queue_t
+  int index;
+  // Other fields can be placed in here
+} queueint_t;
 
 //------------------------------------------------------------------------------
+// Auxiliary Functions
+//------------------------------------------------------------------------------
 
-// imprime na tela um elemento da fila (chamada pela função queue_print)
+// Prints in the screen the elements in of the queue
 void print_elem(void *ptr) {
-  filaint_t *elem = ptr;
+  queueint_t *elem = ptr;
 
-  if (!elem)
+  if (!elem) {
     return;
+  }
 
-  elem->prev ? printf("%d", elem->prev->id) : printf("*");
-  printf("<%d>", elem->id);
-  elem->next ? printf("%d", elem->next->id) : printf("*");
+  elem->prev ? printf("%d", elem->prev->index) : printf("*");
+  printf("<%d>", elem->index);
+  elem->next ? printf("%d", elem->next->index) : printf("*");
 }
 
-//------------------------------------------------------------------------------
-
-// retorna 1 se a estrutura da fila está correta, 0 senão
-int fila_correta(filaint_t *fila) {
-  filaint_t *aux;
-
-  // uma fila vazia sempre está correta
-  if (!fila)
-    return 1;
-
-  // fila com um só elemento e correta
-  if ((fila->next == fila) && (fila->prev == fila))
-    return 1;
-
-  // fila com um só elemento, mas incorreta
-  if ((fila->next == fila) || (fila->prev == fila)) {
-    printf("ERRO: ponteiros errados na fila com um elemento\n");
+// Returns 0 if the queue is correct, 1 otherwise
+int check_queue(queueint_t *queue) {
+  // Empty queue is always correct
+  if (!queue) {
     return 0;
   }
 
-  // fila com mais elementos, percorrer e testar todos os ponteiros
-  aux = fila;
+  // Queue with only one element and correct
+  if ((queue->next == queue) && (queue->prev == queue)) {
+    return 0;
+  }
+
+  queueint_t *aux = queue;
   do {
-    // testa ponteiro next (avaliação em curto-circuito)
-    if (aux->next && (aux->next->prev == aux))
-      ; // ponteiro ok
-    else {
-      printf("ERRO: ponteiros errados ->next ou ->next->prev\n");
-      return 0;
+    if (!(aux->next && (aux->next->prev == aux))) {
+      printf("->next is wrong\n");
+      return 1;
     }
 
-    // testa ponteiro prev (avaliação em curto-circuito)
-    if (aux->prev && (aux->prev->next == aux))
-      ; // ponteiro ok
-    else {
-      printf("ERRO: ponteiros errados ->prev ou ->prev->next\n");
-      return 0;
+    if (!(aux->prev && (aux->prev->next == aux))) {
+      printf("->prev is wrong\n");
+      return 1;
     }
+
     aux = aux->next;
-  } while (aux != fila);
+  } while (aux != queue);
 
-  // passou por tudo, estrutura da fila parece estar ok
-  return 1;
+  return 0;
+}
+
+// Creates the elements of the queue
+queueint_t *create_itens() {
+  queueint_t *items = (queueint_t *)calloc(N, sizeof(queueint_t));
+
+  for (int i = 0; i < N; i++) {
+    items[i].index = i;
+    items[i].prev = NULL;
+    items[i].next = NULL;
+  }
+
+  return items;
 }
 
 //------------------------------------------------------------------------------
+// Test Functions
+//------------------------------------------------------------------------------
 
-int main(int argc, char **argv, char **envp) {
-  int i;
+int queue_insert_test() {
+  queueint_t *items = create_itens();
 
-  // inicializa os N elementos
-  for (i = 0; i < N; i++) {
-    item[i].id = i;
-    item[i].prev = NULL;
-    item[i].next = NULL;
+  queueint_t *queue0 = NULL;
+  for (int i = 0; i < N; i++) {
+    if (queue_size((queue_t *)queue0) != i) {
+      printf("INSERT TEST: Incorrect queue size\n");
+      free(items);
+      return 1;
+    }
+
+    queue_append((queue_t **)&queue0, (queue_t *)&(items[i]));
+
+    if (check_queue(queue0) < 0) {
+      printf("INSERT TEST: Queue is incorrect\n");
+      free(items);
+      return 1;
+    }
   }
 
-  // PARTE 1: queue_append e queue_size =======================================
+  free(items);
+  return 0;
+}
 
-  // Teste: inserir N elemementos na fila e verificar a estrutura
-  printf("Testando insercao de %d elementos...\n", N);
-  fila0 = NULL;
-  for (i = 0; i < N; i++) {
-    assert(queue_size((queue_t *)fila0) == i);
-    queue_append((queue_t **)&fila0, (queue_t *)&item[i]);
-    assert(fila_correta(fila0));
+int queue_order_test() {
+  queueint_t *items = create_itens();
+
+  queueint_t *queue0 = NULL;
+  for (int i = 0; i < N; i++) {
+    queue_append((queue_t **)&queue0, (queue_t *)&(items[i]));
   }
 
-  // Teste: contar o numero de elementos na fila e verificar a ordem
-  // dos elementos inseridos
-  printf("Testando tamanho da fila e ordem dos %d elementos...\n", N);
-  aux = fila0;
-  i = 0;
+  // Check if the order is right
+  int index = 0;
+  queueint_t *aux = queue0;
   do {
-    assert(i == aux->id); // testa posição do elemento i
-    i++;
+    if (index != aux->index) {
+      printf("Wrong position [%d] should be [%d]", aux->index, index);
+      free(items);
+      return 1;
+    }
+
     aux = aux->next;
-  } while (aux != fila0);
+    index++;
+  } while (aux != queue0);
+  assert(index == N);
 
-  assert(i == N);
-
-  assert(queue_size((queue_t *)fila0) == N);
-
-  printf("Testes de insercao funcionaram!\n");
-
-  // PARTE 2: queue_remove ====================================================
-
-  // esvazia fila0, retirando sempre o primeiro elemento
-  printf("Remocao %d vezes o primeiro elemento...\n", N);
-  i = 0;
-  while (i < N) {
-    aux = fila0;
-    queue_remove((queue_t **)&fila0, (queue_t *)aux);
-    assert(fila_correta(fila0)); // estrutura continua correta
-    assert(aux->id == i);        // testa ordem do elemento removido
-    assert(aux->prev == NULL);   // testa elemento removido
-    assert(aux->next == NULL);   // testa elemento removido
-    i++;
+  int size = queue_size((queue_t *)queue0);
+  if (size != N) {
+    printf("Wrong queue size received [%d] should be [%d]", size, N);
+    free(items);
+    return 1;
   }
-  assert(fila0 == NULL); // fila deve estar vazia
-  printf("Ok, apos %d remocoes a fila ficou vazia\n", N);
 
-  // reconstroi fila de teste
-  fila0 = NULL;
-  for (i = 0; i < N; i++)
-    queue_append((queue_t **)&fila0, (queue_t *)&item[i]);
+  free(items);
+  return 0;
+}
 
-  // esvazia fila0, retirando sempre o segundo elemento
-  printf("Remocao %d vezes o segundo elemento...\n", N);
-  i = 0;
-  while (i < N) {
-    aux = fila0->next;
-    queue_remove((queue_t **)&fila0, (queue_t *)aux);
-    assert(fila_correta(fila0));      // estrutura continua correta
-    assert(aux->id == ((i + 1) % N)); // testa ordem do elemento removido
-    assert(aux->prev == NULL);        // testa elemento removido
-    assert(aux->next == NULL);        // testa elemento removido
-    i++;
+int queue_remove_first_test() {
+  queueint_t *items = create_itens();
+
+  queueint_t *queue0 = NULL;
+  for (int i = 0; i < N; i++) {
+    queue_append((queue_t **)&queue0, (queue_t *)&(items[i]));
   }
-  assert(fila0 == NULL); // fila deve estar vazia
-  printf("Ok, apos %d remocoes a fila ficou vazia\n", N);
 
-  // reconstroi fila de teste
-  fila0 = NULL;
-  for (i = 0; i < N; i++)
-    queue_append((queue_t **)&fila0, (queue_t *)&item[i]);
+  int index = 0;
+  while (index < N) {
+    queueint_t *aux = queue0;
+    queue_remove((queue_t **)&queue0, (queue_t *)aux);
 
-  // esvazia fila0, retirando sempre o último elemento
-  printf("Remocao %d vezes o último elemento...\n", N);
-  i = 0;
-  while (i < N) {
-    aux = fila0->prev;
-    queue_remove((queue_t **)&fila0, (queue_t *)aux);
-    assert(fila_correta(fila0));  // estrutura continua correta
-    assert(aux->id + i == N - 1); // testa ordem do elemento removido
-    assert(aux->prev == NULL);    // testa elemento removido
-    assert(aux->next == NULL);    // testa elemento removido
-    i++;
+    if (check_queue(queue0)) {
+      printf("Queue is not correct\n");
+      free(items);
+      return 1;
+    }
+
+    if (aux->index != index) {
+      printf("Order of elements is wrong, expected [%d] got [%d]\n", index,
+             aux->index);
+      free(items);
+      return 1;
+    }
+
+    if (aux->prev != NULL) {
+      printf("->prev is a NULL pointer");
+      free(items);
+      return 1;
+    }
+
+    if (aux->next != NULL) {
+      printf("->next is a NULL pointer");
+      free(items);
+      return 1;
+    }
+    index++;
   }
-  assert(fila0 == NULL); // fila deve estar vazia
-  printf("Ok, apos %d remocoes a fila ficou vazia\n", N);
 
-  // reconstroi fila de teste
-  fila0 = NULL;
-  for (i = 0; i < N; i++)
-    queue_append((queue_t **)&fila0, (queue_t *)&item[i]);
+  if (queue0 != NULL) {
+    printf("Queue is not NULL");
+    free(items);
+    return 1;
+  }
 
-  // remocoes aleatorias
-  printf("Remocao %d vezes um elemento aleatório...\n", N);
-  while (fila0) {
-    i = rand() % queue_size((queue_t *)fila0);
-    aux = fila0;
-    while (i) {
-      i--;
+  free(items);
+  return 0;
+}
+
+int queue_remove_second_test() {
+  queueint_t *items = create_itens();
+
+  queueint_t *queue0 = NULL;
+  for (int i = 0; i < N; i++) {
+    queue_append((queue_t **)&queue0, (queue_t *)&(items[i]));
+  }
+
+  int index = 0;
+  while (index < N) {
+    queueint_t *aux = queue0->next;
+    queue_remove((queue_t **)&queue0, (queue_t *)aux);
+
+    if (check_queue(queue0)) {
+      printf("Queue is not correct\n");
+      free(items);
+      return 1;
+    }
+
+    if (aux->index != ((index + 1) % N)) {
+      printf("Order of elements is wrong, expected [%d] got [%d]\n",
+             ((index + 1) % N), aux->index);
+      free(items);
+      return 1;
+    }
+
+    if (aux->prev != NULL) {
+      printf("->prev is a NULL pointer");
+      free(items);
+      return 1;
+    }
+
+    if (aux->next != NULL) {
+      printf("->next is a NULL pointer");
+      free(items);
+      return 1;
+    }
+    index++;
+  }
+
+  if (queue0 != NULL) {
+    printf("Queue is not NULL");
+    free(items);
+    return 1;
+  }
+
+  free(items);
+  return 0;
+}
+
+int queue_remove_last_test() {
+  queueint_t *items = create_itens();
+
+  queueint_t *queue0 = NULL;
+  for (int i = 0; i < N; i++) {
+    queue_append((queue_t **)&queue0, (queue_t *)&(items[i]));
+  }
+
+  int index = 0;
+  while (index < N) {
+    queueint_t *aux = queue0->prev;
+    queue_remove((queue_t **)&queue0, (queue_t *)aux);
+
+    if (check_queue(queue0)) {
+      printf("Queue is not correct\n");
+      free(items);
+      return 1;
+    }
+
+    if (aux->index + index != N - 1) {
+      printf("Order of elements is wrong, expected [%d] got [%d]\n", N - 1,
+             aux->index + index);
+      free(items);
+      return 1;
+    }
+
+    if (aux->prev != NULL) {
+      printf("->prev is a NULL pointer");
+      free(items);
+      return 1;
+    }
+
+    if (aux->next != NULL) {
+      printf("->next is a NULL pointer");
+      free(items);
+      return 1;
+    }
+    index++;
+  }
+
+  if (queue0 != NULL) {
+    printf("Queue is not NULL");
+    free(items);
+    return 1;
+  }
+
+  free(items);
+  return 0;
+}
+
+int queue_remove_random_test() {
+  queueint_t *items = create_itens();
+
+  queueint_t *queue0 = NULL;
+  for (int i = 0; i < N; i++) {
+    queue_append((queue_t **)&queue0, (queue_t *)&(items[i]));
+  }
+
+  int index = 0;
+  while (queue0) {
+    index = rand() % queue_size((queue_t *)queue0);
+    queueint_t *aux = queue0;
+    while (index) {
+      index--;
       aux = aux->next;
     }
-    queue_remove((queue_t **)&fila0, (queue_t *)aux);
-  }
-  assert(fila0 == NULL); // fila deve estar vazia
-  printf("Ok, apos %d remocoes aleatorias a fila ficou vazia\n", N);
 
-  printf("Testes de remocao funcionaram!\n");
-
-  // PARTE 3: operações inválidas
-  //=============================================
-
-  // inicializa os N elementos
-  for (i = 0; i < N; i++) {
-    item[i].id = i;
-    item[i].prev = NULL;
-    item[i].next = NULL;
+    queue_remove((queue_t **)&queue0, (queue_t *)aux);
   }
 
-  // preparar filas de teste
-  fila0 = NULL;
-  fila1 = NULL;
-  queue_append((queue_t **)&fila0, (queue_t *)&item[0]);
-  queue_append((queue_t **)&fila1, (queue_t *)&item[1]);
-
-  // tentar remover elemento que está em outra fila
-  printf("Testando remocao de elemento que está em outra fila...\n");
-  queue_remove((queue_t **)&fila0, (queue_t *)&item[1]);
-  assert(fila0 == &item[0]);
-  assert(item[0].prev == &item[0]);
-  assert(item[0].next == &item[0]);
-  assert(item[1].prev == &item[1]);
-  assert(item[1].next == &item[1]);
-  printf("Ok, nao deixou remover um elemento de outra fila\n");
-
-  // tentar remover elemento que não está em nenhuma fila
-  printf("Testando remocao de elemento que não está em nenhuma fila...\n");
-  queue_remove((queue_t **)&fila0, (queue_t *)&item[2]);
-  assert(fila0 == &item[0]);
-  assert(item[0].prev == &item[0]);
-  assert(item[0].next == &item[0]);
-  assert(item[2].prev == NULL);
-  assert(item[2].next == NULL);
-  printf("Ok, nao deixou remover um elemento que não está em nenhumafila\n");
-
-  // // tentar inserir algo que já está na mesma fila
-  printf("Testando insercao de elemento que já está na fila...\n");
-  queue_append((queue_t **)&fila0, (queue_t *)&item[0]);
-  assert(fila0 == &item[0]);
-  assert(item[0].prev == &item[0]);
-  assert(item[0].next == &item[0]);
-  printf("Ok, não deixou inserir elemento que já estava na fila\n");
-
-  // // tentar inserir algo que está em outra fila
-  printf("Testando insercao de elemento que está em outra fila...\n");
-  queue_append((queue_t **)&fila0, (queue_t *)&item[1]);
-  assert(fila0 == &item[0]);
-  assert(item[0].prev == &item[0]);
-  assert(item[0].next == &item[0]);
-  assert(fila1 == &item[1]);
-  assert(item[1].prev == &item[1]);
-  assert(item[1].next == &item[1]);
-  printf("Ok, não deixou inserir elemento que está em outra fila\n");
-
-  // // criar uma grande fila com entradas dinamicas
-  fila0 = NULL;
-  for (i = 0; i < N * N; i++) {
-    aux = (filaint_t *)malloc(sizeof(filaint_t));
-    aux->id = i;
-    aux->prev = aux->next = NULL;
-    queue_append((queue_t **)&fila0, (queue_t *)aux);
-    assert(fila_correta(fila0));
-  }
-  printf("Ok, criei uma fila com %d elementos ordenados\n", N * N);
-
-  // retirar e destruir cada elemento da fila, em sequencia
-  for (i = 0; i < N * N; i++) {
-    aux = fila0;
-    queue_remove((queue_t **)&fila0, (queue_t *)fila0);
-    assert(fila_correta(fila0));
-    assert(aux->id == i);
-    free(aux);
-  }
-  printf("Ok, retirei e destrui em ordem %d elementos da fila\n", N * N);
-
-  printf("Testes de operações inválidas funcionaram!\n");
-
-  // PARTE 4: queue_print
-  // =====================================================
-
-  printf("Teste do queue_print...\n");
-
-  // inicializa os N elementos
-  for (i = 0; i < N; i++) {
-    item[i].id = i;
-    item[i].prev = NULL;
-    item[i].next = NULL;
+  if (queue0 != NULL) {
+    printf("Queue is not NULL");
+    free(items);
+    return 1;
   }
 
-  // uma fila vazia
-  fila0 = NULL;
+  free(items);
+  return 0;
+}
 
-  // imprimir a fila
-  printf("Saida esperada: []\n");
-  queue_print("Saida gerada  ", (queue_t *)fila0, print_elem);
+//------------------------------------------------------------------------------
+// Main Functions
+//------------------------------------------------------------------------------
 
-  // uma fila com 10 elementos
-  for (i = 0; i < 10; i++)
-    queue_append((queue_t **)&fila0, (queue_t *)&item[i]);
+int main() {
+  if (queue_insert_test()) {
+    printf("TEST FAILED: queue_insert_test\n");
+    return 1;
+  }
 
-  // imprimir a fila
-  printf("Saida esperada: [9<0>1 0<1>2 1<2>3 2<3>4 3<4>5 4<5>6 5<6>7 6<7>8 "
-         "7<8>9 8<9>0]\n");
-  queue_print("Saida gerada  ", (queue_t *)fila0, print_elem);
+  if (queue_order_test()) {
+    printf("TEST FAILED: queue_order_test\n");
+    return 1;
+  }
 
-  printf("Testes concluidos!!!\n");
+  if (queue_remove_first_test()) {
+    printf("TEST FAILED: queue_remove_first_test\n");
+    return 1;
+  }
 
-  exit(0);
+  if (queue_remove_second_test()) {
+    printf("TEST FAILED: queue_remove_second_test\n");
+    return 1;
+  }
+
+  if (queue_remove_last_test()) {
+    printf("TEST FAILED: queue_remove_last_test\n");
+    return 1;
+  }
+
+  if (queue_remove_random_test()) {
+    printf("TEST FAILED: queue_remove_random_test\n");
+    return 1;
+  }
+
+  return 0;
 }
