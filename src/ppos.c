54bb6c6186d199ace13d868a431bb5cf78ed995b
Victor Briganti <jvbriganti@gmail.com>
2024-09-11 16:38:37 -0300

    [Task][Refactor] Executing task removed from queue
    The executing task is removed from the ready queue before starting to
    execute.

--- src/ppos.c
+++ src/ppos.c
@@ -17,8 +17,11 @@
 #include <sys/ucontext.h>
 #include <ucontext.h>
 
+// Reserved IDs for special Tasks
+#define MAIN_TASK 0
+
 static TaskManager *readyQueue = NULL;
-static task_t *currentTask = NULL;
+static task_t *executingTask = NULL;
 static int threadCount = 0;
 
 //=============================================================================
@@ -28,7 +31,7 @@ static int threadCount = 0;
 void ppos_init() {
   // Removes the virtual buffer of the output
   // https://en.cppreference.com/w/c/io/setvbuf
-  setvbuf(stdout, 0, _IONBF, 0);
+  (void)setvbuf(stdout, 0, _IONBF, 0);
 
   log_set(stderr, 1, LOG_TRACE);
 
@@ -38,24 +41,15 @@ void ppos_init() {
     exit(-1);
   }
 
-  log_debug("%s creating main task", __func__);
-  currentTask = malloc(sizeof(task_t));
-  if (currentTask == NULL) {
+  log_debug("%s allocating main task", __func__);
+  executingTask = malloc(sizeof(task_t));
+  if (executingTask == NULL) {
     log_error("%s failed to allocate main task", __func__);
     exit(-1);
   }
 
-  currentTask->next = NULL;
-  currentTask->prev = NULL;
-  currentTask->tid = threadCount;
-  currentTask->status = TASK_EXEC;
-  currentTask->stack =
-      NULL; // The main task does not need to allocate the stack
-  getcontext(&(currentTask->context));
-  threadCount++;
-
-  if (task_manager_insert(readyQueue, currentTask) < 0) {
-    log_debug("%s could not append task to the queue", __func__);
+  if (task_init(executingTask, NULL, NULL) < 0) {
+    log_error("%s main task could not be initialized", __func__);
     exit(-1);
   }
 }
@@ -70,37 +64,44 @@ int task_init(task_t *task, void (*start_routine)(void *), void *arg) {
     return -1;
   }
 
-  if (start_routine == NULL) {
+  if (threadCount != MAIN_TASK && start_routine == NULL) {
     log_error("%s received a start_routine == NULL", __func__);
     return -1;
   }
 
-  task->status = TASK_READY;
+  task->next = NULL;
+  task->prev = NULL;
   task->tid = threadCount;
 
-  // Initialize the context structure and its stack
-  getcontext(&(task->context));
-  char *stack = malloc((size_t)STACKSIZE);
-  if (stack) {
-    task->context.uc_stack.ss_sp = stack;
-    task->context.uc_stack.ss_size = (size_t)STACKSIZE;
-    task->context.uc_stack.ss_flags = 0;
-    task->context.uc_link = 0;
+  if (threadCount == MAIN_TASK) {
+    task->status = TASK_EXEC;
+    task->stack = NULL; // Main task does not need to allocate a stack
+    getcontext(&(task->context));
   } else {
-    log_error("%s stack could not be allocated", __func__);
-    return -1;
-  }
+    task->status = TASK_READY;
 
-  makecontext(&(task->context), (void *)start_routine, 1, arg);
+    // Initialize the context structure and its stack
+    getcontext(&(task->context));
+    char *stack = malloc((size_t)STACKSIZE);
+    if (stack) {
+      task->context.uc_stack.ss_sp = stack;
+      task->context.uc_stack.ss_size = (size_t)STACKSIZE;
+      task->context.uc_stack.ss_flags = 0;
+      task->context.uc_link = 0;
+    } else {
+      log_error("%s stack could not be allocated", __func__);
+      return -1;
+    }
+    makecontext(&(task->context), (void *)start_routine, 1, arg);
 
-  if (task_manager_insert(readyQueue, task) < 0) {
-    log_debug("%s task(%d) could not be appended in the ready queue", __func__,
-              task->tid);
-    return -1;
+    if (task_manager_insert(readyQueue, task) < 0) {
+      log_debug("%s task(%d) could not be appended in the ready queue",
+                __func__, task->tid);
+      return -1;
+    }
   }
 
   threadCount++;
-
   return 0;
 }
 
@@ -110,49 +111,54 @@ int task_switch(task_t *task) {
     return -1;
   }
 
-  // Search the task in the queue
-  const task_t *aux = readyQueue->taskQueue;
-  do {
-    if (aux->tid == task->tid) {
-      currentTask->status = TASK_READY;
-      task->status = TASK_EXEC;
+  log_debug("%s (%d)->(%d)", __func__, executingTask->tid, task->tid);
 
-      log_debug("%s swapping task(%d) -> task(%d)", __func__, currentTask->tid,
-                task->tid);
-      task_t *temp = currentTask;
-      currentTask = task;
-      swapcontext(&(temp->context), &(currentTask->context));
+  task_manager_print(readyQueue);
+  if (task_manager_remove(readyQueue, task) < 0) {
+    log_debug("%s could not remove task(%d) from ready queue", __func__,
+              task->tid);
+    return -1;
+  }
+  task_manager_print(readyQueue);
 
-      return 0;
-    }
+  if (task_manager_insert(readyQueue, executingTask) < 0) {
+    log_debug("%s could not insert task(%d) into ready queue", __func__,
+              executingTask->tid);
+    return -1;
+  }
+  task_manager_print(readyQueue);
 
-    aux = aux->next;
-  } while (aux != readyQueue->taskQueue);
+  task_t *temp = executingTask;
+  executingTask = task;
+  task->status = TASK_EXEC;
+  temp->status = TASK_READY;
 
-  log_debug("%s task(%d) not found in the queue", __func__, task->tid);
-  return -1;
+  swapcontext(&(temp->context), &(executingTask->context));
+  return 0;
 }
 
 void task_exit(int exit_code) {
-  log_debug("%s removing task(%d) from queue", __func__, currentTask->tid);
-  if (task_manager_remove(readyQueue, currentTask) < 0) {
-    log_error("%s could not remove the task(%d) from queue", __func__,
-              currentTask->tid);
-    return;
-  }
+  log_debug("%s %d", __func__, executingTask->tid);
 
   if (readyQueue->count) {
-    currentTask->status = TASK_FINISH;
-    currentTask = readyQueue->taskQueue;
-    currentTask->status = TASK_EXEC;
+    executingTask->status = TASK_FINISH;
 
+    task_t *aux = readyQueue->taskQueue;
+    log_debug("%s executing task(%d)", __func__, aux->tid);
+    task_manager_print(readyQueue);
+    if (task_manager_remove(readyQueue, aux) < 0) {
+      log_error("%s could not get next executing task(%d)", __func__, aux->tid);
+      return;
+    }
     task_manager_print(readyQueue);
 
-    setcontext(&(currentTask->context));
+    aux->status = TASK_EXEC;
+    executingTask = aux;
+    setcontext(&(executingTask->context));
   }
 }
 
 int task_id() {
-  log_debug("%s %d", __func__, currentTask->tid);
-  return currentTask->tid;
+  log_debug("%s %d", __func__, executingTask->tid);
+  return executingTask->tid;
 }
